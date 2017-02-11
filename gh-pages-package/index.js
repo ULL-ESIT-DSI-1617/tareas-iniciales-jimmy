b92bc55902693ea510ea6088c111c7b81497ddfa
Alex Jimmy <alu0100728170@ull.edu.es>
2017-02-11 03:17:52 +0000

    Actualizacion de script para realizar el commit, en la rama gh-pages, con mensaje de forma obligatoria

--- gh-pages-package/index.js
+++ gh-pages-package/index.js
@@ -1,10 +1,16 @@
 'use strict'
 var ghpages = require('gh-pages');
-
-ghpages.publish(`${__dirname}/../_book`, (error)=> {
-    if(error) {
-        console.log('Algo salió mal :(');
-    }else {
-        console.log('gh-pages actualizado');
-    }
-});
+var mensaje_commit = process.argv[2];
+if(mensaje_commit){
+    ghpages.publish(`${__dirname}/../_book`,{
+        message: mensaje_commit
+        } ,(error)=> {
+        if(error) {
+            console.error('Algo salió mal :(');
+        }else {
+            console.log('gh-pages actualizado');
+        }
+    });
+}else {
+    console.error('Indica el mensaje de commit');
+}
