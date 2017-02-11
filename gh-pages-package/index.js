e2cb8dfa9f331c6d5e001b58edffe12df38ecc52
Alex Jimmy <alu0100728170@ull.edu.es>
2017-02-11 19:52:53 +0000

    Apartado Github Pages creado

--- gh-pages-package/index.js
+++ gh-pages-package/index.js
@@ -1,10 +1,18 @@
 'use strict'
 var ghpages = require('gh-pages');
+var mensaje_commit = process.argv[2];
+if(mensaje_commit){
+    ghpages.publish(`${__dirname}/../_book`,{
+        message: mensaje_commit
+        } ,(error)=> {
+        if(error) {
+            console.error('Algo salió mal :(');
+            console.error(error.message);
+        }else {
+            console.log('gh-pages actualizado');
 
-ghpages.publish(`${__dirname}/../_book`, (error)=> {
-    if(error) {
-        console.log('Algo salió mal :(');
-    }else {
-        console.log('gh-pages actualizado');
-    }
-});
+        }
+    });
+}else {
+    console.error('Indica el mensaje de commit');
+}
